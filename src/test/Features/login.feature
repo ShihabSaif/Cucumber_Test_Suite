7c4e2928f7156f608a783076202a91a71c7c7fcd
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2025-06-28 02:11:08 +0600

    test runner & auto report generating section added

--- src/test/Features/login.feature
+++
@@ -1,8 +0,0 @@
-Feature: feature to test login functionality
-  Scenario Outline: check login is successful with valid credentials
-    Given user is on login page
-    When user enters <username> and <password>
-    And click login button
-    Then user is navigated to the homepage
-    Examples:
-      | username | password |
---
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -0,0 +1,28 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
+        System.out.println("given block 1");
+    }
+
+    @When("user enters username and password")
+    public void userEntersUsernameAndPassword() {
+        System.out.println("when block 2");
+    }
+
+    @And("click login button")
+    public void clickLoginButton() {
+        System.out.println("and block 3");
+    }
+
+    @Then("user is navigated to the homepage")
+    public void userIsNavigatedToTheHomepage() {
+        System.out.println("then block 4");
+    }
+}
